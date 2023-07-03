0726442eff87c20f938e9c6fa29aef9d279d87ba
Jean F. dos Santos <jeanddg@hotmail.com>
2023-07-03 19:14:22 -0300

    feat: add migrations account, category

--- user_profile/models.py
+++ user_profile/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    essential = models.BooleanField(default=False)
+    budget_price = models.FloatField(default=0)
+    
+    def __str__ (self): 
+        return self.name
+    
+
+class Account(models.Model):
+    bank_list = (
+        ('NU', 'Nubank'),
+        ('CE', 'Caixa Econômica'),
+        ('BR', 'Banco do Brasil'),
+        ('IT', 'Itaú'),
+        ('SF', 'Banco Safra'),
+        ('SG', 'Santander Group')        
+    )
+    
+    types_of_person = (
+        ('PF', 'Pessoa Física'),
+        ('PJ', 'Pessoa Jurídica')
+    )
+    
+    nickname = models.CharField(max_length=50)
+    bank = models.CharField(max_length=2, choices=bank_list)
+    type_of_person = models.CharField(max_length=2, choices=types_of_person)
+    value = models.FloatField()
+    icon = models.ImageField(upload_to='icons')
+    
+    def __str__ (self): 
+        return self.nickname
