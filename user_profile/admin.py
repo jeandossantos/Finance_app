ad7f536e032ff776ee6fb236ee2584b3acd32fe0
Jean F. dos Santos <jeanddg@hotmail.com>
2023-07-04 21:18:06 -0300

    feat: configure account and category admin

--- user_profile/admin.py
+++ user_profile/admin.py
@@ -2,6 +2,13 @@ from django.contrib import admin
 from .models import Account, Category
 # Register your models here.
 
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'essential', 'budget_price')
 
-admin.site.register(Account),
-admin.site.register(Category)
+admin.site.register(Category, CategoryAdmin)
+
+
+class AccountAdmin(admin.ModelAdmin):
+    list_display = ('nickname','bank', 'type_of_person', 'value', 'icon', 'created_at')
+
+admin.site.register(Account, AccountAdmin)
