39ed0fa3637fe70e7b51ab33d1e1b4dc7c947226
Jean F. dos Santos <jeanddg@hotmail.com>
2023-07-05 10:34:20 -0300

    feat: add extract app

--- core/settings.py
+++ core/settings.py
@@ -27,7 +27,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'user_profile'
+    'user_profile',
+    'extract'
 ]
 
 MIDDLEWARE = [
--- core/urls.py
+++ core/urls.py
@@ -5,5 +5,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('profile/', include('user_profile.urls')),    
+    path('profile/', include('user_profile.urls')),
+    path('extract/', include('extract.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
