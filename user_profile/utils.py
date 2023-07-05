f29ac909a61f29c51c4bd82aa0596e0af1a5a733
Jean F. dos Santos <jeanddg@hotmail.com>
2023-07-04 21:18:20 -0300

    feat: create utils

---
+++ user_profile/utils.py
@@ -0,0 +1,11 @@
+
+def format_price(price):
+    return "{:.2f}".format(price)
+
+def getTotalPrice(objs, key):
+    total = 0
+    
+    for obj in objs:
+        total += getattr(obj, key)
+    
+    return format_price(total)
